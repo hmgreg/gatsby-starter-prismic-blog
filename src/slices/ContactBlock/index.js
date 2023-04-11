aaf76ada9c7c501110b86d93be0119c192436e2e
hmgreg <greg@happymonday.co.nz>
2023-04-12 11:05:07 +1200

    Revert "Added Contacts Slice to Page"
    This reverts commit 911044576beae352867eac16e4f1e85a36456194.

---
+++ src/slices/ContactBlock/index.js
@@ -0,0 +1,58 @@
+import { Bounded } from "../../components/Bounded";
+//import * as prismicH from "@prismicio/helpers";
+import { PrismicRichText } from "@prismicio/react";
+
+
+const ContactBlock = ({ slice }) => {
+  const image = slice.primary.title;
+
+  return (
+    <Bounded as="section" size="base">
+      <figure>
+         
+            <PrismicRichText field={image} />
+        
+      </figure>
+    </Bounded>
+  );
+};
+// todo: repater type field
+/*
+const ContactBlockRepeater = () => {
+  const name = slice.items[i].name;
+  const position = slice.items[i].position;
+
+  return (
+    <Bounded as="section" size="small">
+      <form
+        action="/api/contact"
+        method="post"
+        className="grid grid-cols-1 gap-6"
+      >
+        <InputField label="Name" name="name" placeholder={name} />
+        <InputField
+          label="Email Address"
+          name="email"
+          type="email"
+          placeholder={position}
+        />
+        <TextareaField
+          label="Message"
+          name="message"
+          placeholder="Placeholder"
+        />
+        <button
+          type="submit"
+          className="ml-auto inline-flex items-center gap-2"
+        >
+          Send message{" "}
+          <span aria-hidden={true} className="text-xl">
+            &rarr;
+          </span>
+        </button>
+      </form>
+    </Bounded>
+  );
+};
+*/
+export default ContactBlock;
